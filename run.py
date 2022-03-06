8ada9b6ffaeb610812090d4f235b0ff3874c7af4
ChrisCherng <christopher.cherng@gmail.com>
2022-03-06 15:03:51 +0000

    add key basic functions for the app

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from prettytable import PrettyTable
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -73,9 +74,29 @@ def validate_view_function(response):
 def select_amend_function():
     """
     If the user has selected amend in the first question
-    This function allows the user to select which type of amendment they would like to make
+    This function allows the user to select which type of amendment they want
     """
-    print("Test")
+    while True:
+        print("What amendment would you like?")
+
+        amend_response = input("Type 'Add', 'Delete' or 'Complete' here: \n")
+        if validate_amend_function(amend_response):
+            break
+
+    return amend_response
+
+
+def validate_amend_function(response):
+    """
+    Checks that the user has input 'Add', 'Delete' or 'Complete'
+    Allows the user to input either upper or lower case
+    """
+    amendments = ['add', 'delete', 'complete']
+    if response.lower() in amendments:
+        return True
+    else:
+        print("Response should be 'Add', 'Delete' or 'Complete' only")
+        return False
 
 
 def view_full():
@@ -94,15 +115,65 @@ def view_summary():
     """
     Formats and displays a summary of the list
     """
-    print("Test")
+    data = SHEET.worksheet('summary').get_all_values()
+    summary_list = PrettyTable()
+    summary_list.field_names = data[0]
+    for i in range(len(data)-1):
+        summary_list.add_row(data[i+1])
+    print(summary_list)
 
 
-if select_function().lower() == 'view':
-    select_view_function()
-elif select_function().lower() == 'amend':
-    select_amend_function()
+def add_task():
+    """
+    Requests the user to input a new task
+    Adds the task to the google sheet list at the end
+    """
+    new_input = input("Type the name of the task here: \n")
+    print("What is the deadline for this new task?")
+    new_date = input("Use the format DD/MM/YYYY?: \n")
+    validate_add_task(new_date)
 
-if select_view_function().lower() == 'full':
-    view_full()
-elif select_view_function().lower() == 'summary':
-    view_summary()
+    new_task = ['X', new_input, new_date, 'Incomplete']
+    list_worksheet = SHEET.worksheet('to_do')
+    list_worksheet.append_row(new_task)
+
+
+def validate_add_task(date):
+    """
+    Validates that the date input by the user is in the correct format
+    Will provide an error message if incorrect and request the date again
+    """
+    date_format = "%d/%m/%y"
+    res = True
+    try:
+        res = bool(datetime.strptime(date, date_format))
+    except ValueError:
+        res = False
+
+    return res
+
+
+def delete_task():
+    """
+    Requests the user input a task number from the full table list
+    Deletes the task from the google sheet
+    """
+    list_worksheet = SHEET.worksheet('to_do')
+    task_numbers = list_worksheet.col_values(1)
+    task_selection = input("Which task number would you like to delete? \n")
+    task_position = int(task_numbers.index(task_selection))
+    list_worksheet.delete_rows(task_position + 1)
+    print(f"Task number {task_selection} has been deleted!")
+
+
+def complete_task():
+    """
+    Requests the user input a task number from the full table list
+    Sets the task status to Complete
+    """
+    list_worksheet = SHEET.worksheet('to_do')
+    task_numbers = list_worksheet.col_values(1)
+    task_selection = input("Which task number is complete? \n")
+    task_position = int(task_numbers.index(task_selection))
+    list_worksheet.update_cell(task_position + 1, 4, 'Complete')
+    print(f"Task number {task_selection} has been set to Complete!")
